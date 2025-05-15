75476d7db5b9a68a7088925dd20e1911d0b99a6e
Kirti Bhosale <bhosalekirti2002@gmail.com>
2025-05-15 11:41:59 +0530

    Question 03 copleted

--- Question03.js
+++ Question03.js
@@ -8,8 +8,34 @@
 // Output:
 // Return a list of total maintenance costs for each query.
 // Example Input:
-// maintenance_logs = [(101, "2024-01-01", 500), (102, "2024-01-10", 300), (101, "2024-01-15", 700)]
-// queries = [("2024-01-01", "2024-01-10"), ("2024-01-01", "2024-01-15")]
+var n = 3;
+var maintenance_logs = [
+  [101, "2024-01-01", 500],
+  [102, "2024-01-10", 300],
+  [101, "2024-01-15", 700],
+];
+var queries = [
+  ["2024-01-01", "2024-01-10"],
+  ["2024-01-01", "2024-01-15"],
+];
 
 // Example Output:
 // [800, 1500]  # Total maintenance cost in the given date range
+
+function maintenanceLog(maintenance_logs, queries) {
+  const res = [];
+
+  for (let [start_date, end_date] of queries) {
+    let total = 0;
+
+    for (let [equipment_id, date, cost] of maintenance_logs) {
+      if (date >= start_date && date <= end_date) {
+        total += cost;
+      }
+    }
+    res.push(total);
+  }
+  return res;
+}
+
+console.log(maintenanceLog(maintenance_logs, queries));
